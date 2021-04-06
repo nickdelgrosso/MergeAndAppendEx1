f83b273f7881c5b11cddf8c5b2e5f02c28651d79
Murali546 <murali5046@gmail.com>
2021-04-06 15:52:25 +0530

    task2 completed!

--- scripts/morning/room2/2_sleep.py
+++ scripts/morning/room2/2_sleep.py
@@ -1,5 +1,4 @@
 
-
 """
 # Exercise:
 Humans in Europe are expected to live ~81 years.  But they spend a lot that time sleeping--silly humans!
@@ -10,10 +9,15 @@ What's the average ESE of your group?
 """
 
 # Raw Data:
-sleep_per_night = []  # e.g. [6.5, 9.2, 8, ...]
+sleep_per_night = [7, 8.5,8]  # e.g. [6.5, 9.2, 8, ...]
 
 # Script (fill in here):
+ese_years=[]
+for sleep in sleep_per_night:
+    ese_year=81*sleep/24
+    ese_years.append(ese_year)
+
 
     
 # Output:
-# print(ese_years)
+print(ese_years)
